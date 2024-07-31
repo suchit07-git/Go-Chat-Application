6c39096fe5a0edc613c578791bcd476035cf0493
Suchit <suchitkarunakaran@gmail.com>
2024-08-01 01:22:04 +0530

    Improved frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,37 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./ws/index.js";
 import Header from "./components/Header/Header.jsx";
-import Message from "./components/Messages/Message.jsx";
+import ChatHistory from "./components/ChatHistory/ChatHistory.jsx";
+import ChatInput from "./components/ChatInput/ChatInput.jsx";
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			messages: []
+			chatHistory: []
 		}
 	}
 	componentDidMount() {
 		connect((msg) => {
-			console.log("New message");
+			console.log("New Message")
 			this.setState(prevState => ({
-				messages: [...this.state.messages, msg]
+				chatHistory: [...this.state.chatHistory, msg]
 			}))
+			console.log(this.state);
 		});
 	}
-	send() {
-		console.log("Hello from client!");
-		sendMsg("Hello from client!");
+	send(e) {
+		if (e.keyCode === 13) {
+			sendMsg(e.target.value);
+			e.target.value = "";
+		}
 	}
 	render() {
 		return (
 			<div className="App">
 				<Header />
-				<Message messages={this.state.messages} />
-				<button onClick={this.send}>Hit</button>
+				<ChatHistory chatHistory={this.state.chatHistory} />
+				<ChatInput send={this.send} />
 			</div>
 		);
 	}
--- frontend/src/components/Messages/index.js
+++
@@ -1,1 +0,0 @@
-import Messages from "./Messages.jsx"
